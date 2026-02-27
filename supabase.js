4cb97f8435b60133e1c633675a626de9c354c99b
mohammedalaa89 <34958470+mohammedalaa89@users.noreply.github.com>
2026-02-27 06:20:25 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,10 +1,20 @@
-// ضع بياناتك هنا
+
 const SUPABASE_URL = "https://rfuifdpjlyhqvcywdzrg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmdWlmZHBqbHlocXZjeXdkenJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNDU2MDIsImV4cCI6MjA4NzYyMTYwMn0.bNyi-l5kx6y6TsXayP0oJ9iwchPneEaCiZ9R8l0oMBE"
 
-// تأكد أن مكتبة supabase اتحمّلت من الـ CDN قبل ما ينفذ هذا الملف
-if (!window.supabase || !window.supabase.createClient) {
-  console.error("Supabase CDN not loaded. Make sure you included the CDN script before supabase.js");
+// في CDN v2 اسم الكائن هو supabase
+if (typeof supabase === "undefined") {
+  console.error("Supabase CDN not loaded!");
 } else {
-  window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+  window.supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY,
+    {
+      auth: {
+        persistSession: true,
+        autoRefreshToken: true,
+        detectSessionInUrl: true
+      }
+    }
+  );
 }
