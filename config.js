fa8e55190c161af46700f8a744285f9182fec64a
mohammedalaa89 <34958470+mohammedalaa89@users.noreply.github.com>
2026-02-27 03:30:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Paste your n8n webhook URL (Production URL)
 // Example: https://your-n8n-domain/webhook/client-intake
 window.INTAKE_WEBHOOK_URL = "https://quadp.app.n8n.cloud/webhook/client-intake";
+window.INTAKE_SECRET = "Quadp_P@ssw0rd2026";
