f8d90ce51f3501990b1f57ad1243726c45d84678
mohammedalaa89 <34958470+mohammedalaa89@users.noreply.github.com>
2026-02-27 06:01:24 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,11 +1,10 @@
-// استدعاء مكتبة Supabase من CDN
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-// ⚠️ ضع بيانات مشروعك هنا
+// ضع بياناتك هنا
 const SUPABASE_URL = "https://rfuifdpjlyhqvcywdzrg.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmdWlmZHBqbHlocXZjeXdkenJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwNDU2MDIsImV4cCI6MjA4NzYyMTYwMn0.bNyi-l5kx6y6TsXayP0oJ9iwchPneEaCiZ9R8l0oMBE"
-// إنشاء العميل
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-// اجعله متاحًا عالميًا
-window.supabaseClient = supabase
+// تأكد أن مكتبة supabase اتحمّلت من الـ CDN قبل ما ينفذ هذا الملف
+if (!window.supabase || !window.supabase.createClient) {
+  console.error("Supabase CDN not loaded. Make sure you included the CDN script before supabase.js");
+} else {
+  window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+}
